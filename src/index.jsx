3de512a9c3eecf54a1f87e186a7029dffd738a61
jilan-text-account <160491810+jilan-text-account@users.noreply.github.com>
2025-02-27 13:21:44 +0530

    Update index.jsx
    cleared data

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,1 @@
-import React from "react";
-import ReactDOM from "react-dom";
-//import App from "./components/App";
 
-ReactDOM.render(<h1>hello</h1>, document.getElementById("root"));
-
-//ReactDOM.render(<App />, document.getElementById("root"));
